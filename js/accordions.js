2f423b33d949a069fa388de3d194bcc2bb2dbc27
chillson2023 <chillson2023@k12.andoverma.us>
2019-12-20 11:38:43 -0500

    uo

--- js/accordions.js
+++ js/accordions.js
@@ -1,4 +1,28 @@
+/* accordion function 1*/
+
 function accordionFunction(id) {
+  var x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+/* accordion function 2 */
+
+function accordionFunction2(id) {
+  var x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+/* accordion function 3*/
+
+function accordionFunction3(id) {
   var x = document.getElementById(id);
   if (x.className.indexOf("w3-show") == -1) {
     x.className += " w3-show";
